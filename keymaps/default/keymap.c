192d3928a7cf3622ae569cabe526238926edcc0f
repo <reporepo254@gmail.com>
2024-04-07 18:17:20 +0900

    remove comment

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,17 +4,6 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
     [0] = LAYOUT(
                 KC_A,   KC_B,   KC_C,   KC_D,
                 KC_E,   KC_F,   KC_G,   KC_H,
